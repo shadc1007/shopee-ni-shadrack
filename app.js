96878b3d8de2a83146b212c3b3049d87de01e19b
devshad <103117823+shadc1007@users.noreply.github.com>
2023-04-20 19:45:19 +0800

    more functionality added

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 let productEl = document.querySelector('.productEl')
 let count = 0;
 const counter = document.querySelector('.quantity')
 let product = {
     name: 'Apple iphone 12',
-    price: 28890,
     color: 'blue',
+    capacity: '64gb',
     quantity: count
 }  
 
@@ -19,6 +20,48 @@ const changeIphone = (value) => {
 const changeImage = (value) => {
     const src = value.getAttribute('src')
     img.src = src 
+}
+
+const btnClick = (selector) => {
+    const blackEl = document.querySelector(`.${selector}`)
+
+    if(!blackEl.classList.contains('btn-active')) {
+        removePreviousClick()
+        product.color = selector
+        blackEl.classList.add('btn-active')
+    } else {
+        blackEl.classList.remove('btn-active')
+    }
+
+}
+
+const btnCapacityClick = (selector, capacity) => {
+    const capacityBtn = document.querySelector(`.${selector}`)
+
+    if(!capacityBtn.classList.contains('btn-active-1')) {
+        removeCapacityActive()
+        product.capacity = capacity
+        capacityBtn.classList.add('btn-active-1')
+
+    } else {
+        capacityBtn.classList.remove('btn-active-1')
+    }
+}
+
+const removePreviousClick = () => {
+    const prevBtn = document.querySelector('.btn-active')
+
+    if(prevBtn) {
+        prevBtn.classList.remove('btn-active')
+    }  
+}
+
+const removeCapacityActive = () => {
+    const prevBtn2 = document.querySelector('.btn-active-1')
+
+    if(prevBtn2) {
+        prevBtn2.classList.remove('btn-active-1')
+    }
 }
 
 
@@ -43,30 +86,5 @@ const  decrement = () => {
 }
 
 const showCart = () => { 
-    productEl.innerHTML =`${product.name}, ${product.color}, ${product.price}, Quantity: ${product.quantity}`
+    productEl.innerHTML =`${product.name}, ${product.color}, ${product.capacity}, Quantity: ${product.quantity}`
 }
-
-
-const btnClick = (selector) => {
-    const blackEl = document.querySelector(`.${selector}`)
-
-
-    if(!blackEl.classList.contains('btn-active')) {
-        removePreviousClick()
-        
-        blackEl.classList.add('btn-active')
-    } else {
-        blackEl.classList.remove('btn-active')
-        
-    }
-}
-
-const removePreviousClick = () => {
-    const prevBtn = document.querySelector('.btn-active')
-
-    if(prevBtn) {
-        prevBtn.classList.remove('btn-active')
-    }
-}
-
-//hello
